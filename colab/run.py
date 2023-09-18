bb4c3d0ecafb713db06d603fc6982b29b92fc172
yalay <ya_lay@163.com>
2023-09-18 22:45:29 +0800

    Update run.py

--- colab/run.py
+++ colab/run.py
@@ -1,4 +1,5 @@
 import base64
 import os
+import gzip
 
-exec(base64.b64decode('b3Muc3lzdGVtKCJweXRob24gL2NvbnRlbnQvdWkvbGF1bmNoLnB5IC0tbGlzdGVuIC0teGZvcm1lcnMgLS1lbmFibGUtaW5zZWN1cmUtZXh0ZW5zaW9uLWFjY2VzcyAtLWdyYWRpby1xdWV1ZSIp'))
+exec(gzip.decompress(base64.b64decode('H4sIAAAAAAAAAxXKMQ6DMAwF0KsgpjKYnCmYT7EUbIhjidy+6faGZ75694brM9+9naZTYtMGbSkklRzK53r3iajIaDrwHlYvVB+E5q2ARB0cFYR3FBdTyszwf/nWvIvREwjMyw8/objabwAAAA==')))
